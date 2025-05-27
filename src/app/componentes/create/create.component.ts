2d375f16aeac6831c170254ee27ace6c3b24b6f4
renato <negreterenato@gmail.com>
2025-05-27 17:48:44 -0500

    second commit

--- src/app/componentes/create/create.component.ts
+++ src/app/componentes/create/create.component.ts
@@ -1,11 +1,34 @@
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { Component } from '@angular/core';
+import { Entidad } from '../../modelo/Entidad';
+import { EntidadService } from '../../servicios/entidad.service';
 
 @Component({
   selector: 'app-create',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './create.component.html',
   styleUrl: './create.component.css'
 })
 export class CreateComponent {
+
+  entidad: Entidad = {
+    id: 0,
+    nombre: '',
+    nit: ''
+  };
+
+  constructor(private entidadService: EntidadService) {}
+
+  async guardarEntidad() {
+    try {
+      console.log(this.entidad)
+      const resultado = await this.entidadService.createAdministrador(this.entidad);
+      console.log('Entidad creada:', resultado);
+      this.entidad = { id: 0, nombre: '', nit: '' };
+    } catch (error) {
+      console.error('Error al crear la entidad:', error);
+    }
+  }
 
 }
--- src/app/componentes/show/show.component.ts
+++ src/app/componentes/show/show.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Entidad } from '../../modelo/Entidad';
+import { EntidadService } from '../../servicios/entidad.service';
 
 @Component({
   selector: 'app-show',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './show.component.html',
   styleUrl: './show.component.css'
 })
-export class ShowComponent {
+export class ShowComponent implements OnInit{
+
+  datosEntidades: Entidad[] = []
+
+  constructor(
+    private entidadService: EntidadService
+  ){}
+
+  ngOnInit(): void {
+      this.cargarEntidades()
+  }
+
+  async cargarEntidades() {
+    try {
+      this.datosEntidades = await this.entidadService.getEntidades();
+    } catch (error) {
+      console.error('Error cargando entidades:', error);
+    }
+  }
 
 }
